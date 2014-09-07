b27048b2cf98465845521e3239fd11cff5ffd936
Aditya <adityakarnam@outlook.com>
2014-09-07 23:16:40 +0530

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 SMP PREEMPT Sat Sep 6 16:01:06 IST 2014"
+#define LINUX_COMPILE_BY "aditya"
+#define LINUX_COMPILE_HOST "aditya-Inspiron-7520"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
